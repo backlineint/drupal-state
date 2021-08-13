2ee418e9df09b5693de212c48a5b738526800dfa
Brian Perry <brian.perry@pantheon.io>
2021-08-13 09:31:09 -0500

    Created initial drupalState class

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,27 @@
-import create from 'zustand/vanilla'
-import './style.css'
+import drupalState from './drupalState';
+import './style.css';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
+
+const blueState: any = new drupalState();
 
 const response = await fetch(`https://live-contentacms.pantheonsite.io/api/recipes`)
   .then(async (response) => {
     return await response.json();
   });
 
+blueState.setState({ response });
+// This is a Zustand store, and you an also add your own custom state.
+blueState.setState({ custom: "custom state" });
+
 /**
  * Next:
+ * Define non-web component API would look like
  * POC with nested Web components - parent that manages state and then 
  * a slot that renders another web component that uses that data. 
  * Something like https://apolloelements.dev/
- * experiment with subscrube and destroy
+ * experiment with subscribe and destroy
  * Define non-web component API would look like
  */
 
-const store = create(() => ({ response }));
-const { getState, setState, subscribe, destroy } = store;
-
-setState({ custom: "custom state"});
-
-app.innerHTML = `<pre>${JSON.stringify(getState(), null, 2)}</pre>`;
+app.innerHTML = `<pre>${JSON.stringify(blueState.getState(), null, 2)}</pre>`;
