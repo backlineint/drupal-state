aa7368b81194779f9ece447f7ca8fd2b5e6171d1
Brian Perry <contact.brianperry@gmail.com>
2021-08-15 09:57:38 -0500

    Set API Root in constructor

--- src/drupalState.ts
+++ src/drupalState.ts
@@ -1,16 +1,24 @@
-import create from 'zustand/vanilla'
+import create from 'zustand/vanilla';
+
+interface DrupalStateConfig {
+  apiRoot: string;
+}
 
 // TODO - Correctly type all of this once model settles.
 class drupalState {
+  apiRoot: string;
   store: any;
   getState: any;
   setState: any;
   subscribe: any;
   destroy: any;
 
-  constructor() {
+  constructor({ apiRoot }: DrupalStateConfig) {
+    this.apiRoot = apiRoot ? apiRoot : '';
+
     this.store = create(() => ({}));
     const { getState, setState, subscribe, destroy } = this.store;
+
     this.getState = getState;
     this.setState = setState;
     this.subscribe = subscribe;
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,18 @@
 import drupalState from './drupalState';
 import './style.css';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>('#app')!;
 
-const blueState: any = new drupalState();
+const blueState: any = new drupalState({
+  apiRoot: 'https://live-contentacms.pantheonsite.io/api/recipes',
+});
 
-const response = await fetch(`https://live-contentacms.pantheonsite.io/api/recipes`)
-  .then(async (response) => {
-    return await response.json();
-  });
+const response = await fetch(blueState.apiRoot).then(async response => {
+  return await response.json();
+});
 
 blueState.setState({ response });
 // This is a Zustand store, and you an also add your own custom state.
-blueState.setState({ custom: "custom state" });
-
-/**
- * Next:
- * Define non-web component API would look like
- * POC with nested Web components - parent that manages state and then 
- * a slot that renders another web component that uses that data. 
- * Something like https://apolloelements.dev/
- * experiment with subscribe and destroy
- * Define non-web component API would look like
- */
+blueState.setState({ custom: 'custom state' });
 
 app.innerHTML = `<pre>${JSON.stringify(blueState.getState(), null, 2)}</pre>`;
