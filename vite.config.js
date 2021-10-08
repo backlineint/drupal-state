4065939850d0c97db27ed1fb21329634ed3ef76b
brianperry <backlineint@2304500.no-reply.drupal.org>
2021-10-08 15:18:42 +0000

    Class properties

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
+const legacy = require('@vitejs/plugin-legacy');
 
 module.exports = {
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'lib.ts'),
