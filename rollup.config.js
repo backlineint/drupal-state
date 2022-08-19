5722d7e6002187835368d25a7e1291f327e19c89
coby.sher <coby.sher@pantheon.io>
2022-08-19 14:30:08 -0500

    Issue #3303412: Pin critical dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,35 @@
 import typescript from '@rollup/plugin-typescript';
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 
+const globals = {
+  'zustand/vanilla': 'zustand',
+  'isomorphic-fetch': 'fetch',
+  '@apollo/client/core': 'apolloCore',
+  'apollo-link-json-api': 'apolloLinkJsonApi',
+  jsona: 'Jsona',
+  'drupal-jsonapi-params': 'drupalJsonapiParams',
+  deepmerge: 'deepmerge',
+  humps: 'humps',
+};
+const external = [
+  'humps',
+  'isomorphic-fetch',
+  '@apollo/client/core',
+  'zustand/vanilla',
+  'apollo-link-json-api',
+  'jsona',
+  'drupal-jsonapi-params',
+  'deepmerge',
+];
+
 export default [
   {
     input: 'lib.ts',
+    external,
     output: {
       file: 'dist/drupal-state.es.js',
       format: 'es',
+      globals,
     },
     plugins: [
       typescript(),
@@ -26,10 +49,12 @@ export default [
   },
   {
     input: 'lib.ts',
+    external,
     output: {
       file: 'dist/drupal-state.umd.js',
       format: 'umd',
       name: 'DrupalState',
+      globals,
     },
     plugins: [typescript()],
   },
